0069776c072e2c7734c0c0173df586588eb1b470
Corinne Marshall <corinnemarshall@Corinnes-MacBook-Air.local>
2021-10-14 14:02:49 -0400

    added roles into seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,14 @@ VALUES
     ('Legal');
 
 
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('Sales Lead', 112987, 1),
+    ('Salesperson', 113987, 1),
+    ('Lead Engineer', 226789, 2),
+    ('Software Engineer', 227789, 2),
+    ('Accountant', 323456, 3),
+    ('Legal Team Lead', 433456, 4),
+    ('Lawyer', 443456, 4);
+
+
