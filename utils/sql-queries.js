6d036b6c256de9dbd2aa24dcc563337caa222fef
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-10-15 15:15:24 -0400

    added ability to view all roles

--- utils/sql-queries.js
+++ utils/sql-queries.js
@@ -15,3 +15,17 @@ viewAllDepartments = () => {
     init();
 };
 
+viewAllRoles = () => {
+    const query = `SELECT roles.id, roles.title, departments.name AS department, roles.salary
+                    FROM roles
+                    LEFT JOIN departments ON roles.department_id = departments.id`;
+    db.query( query, function (err, res) {
+        if (err) {
+            console.log(`Something went wrong" ${err}`);
+            return;
+        }
+        console.table(`\n\nALL ROLES, res`);
+        console.log(`Press UP or DOWN to continue...`);
+    });
+    init();
+};
