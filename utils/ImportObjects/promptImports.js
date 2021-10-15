324ad8826f7125db9f0866a2220e13a0492c1c36
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-10-15 16:59:06 -0400

    added exports for the prompts

---
+++ utils/ImportObjects/promptImports.js
@@ -0,0 +1,17 @@
+const addEmployee = require('../../lib/addEmployee');
+const removeEmployee = require('../../lib/removeEmployee');
+const addRole = require('../../lib/addRole');
+const addDept = require('../../lib/addDepartment');
+const removeDept = require('../../lib/removeDepartment');
+const updateEmpRole = require('../../lib/updateRole');
+const removeEmpRole = require('../../lib/removeRole');
+
+module.exports = {
+    addEmployee,
+    removeEmployee,
+    addRole,
+    addDept,
+    removeDept,
+    updateEmpRole,
+    removeEmpRole
+};
